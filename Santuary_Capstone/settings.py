8eafe2dd1a19de085d777d3593352552413043d1
Mark Dziuban <markdziuban@gmail.com>
2021-10-18 22:46:04 -0400

    updated key

--- Santuary_Capstone/settings.py
+++ Santuary_Capstone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from secret_key import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7j&o6fs#33uid1lqh5*fpwzrbc&yhykk*n6mu!!&_r%n_rj=ol'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
